a911fa0a6cc3949dc139e9f0b32743810a4acb73
Luis Eduardo Altino <ads.luis.sobrinho@gmail.com>
2021-12-01 08:34:27 -0300

    Inline Conversations

--- routes/botman-conversation.php
+++ routes/botman-conversation.php
@@ -1,1 +1,20 @@
 <?php
+
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Messages\Conversations\Conversation;
+use BotMan\BotMan\Messages\Incoming\Answer;
+
+/** @var BotMan $botman */
+$botman = app('botman');
+
+$botman->hears('survey', function (BotMan $bot) {
+    $bot->ask('What is your name?', function (Answer $answer, Conversation $conversation) {
+        $value = $answer->getText();
+        $conversation->say('Nice to meet you, ' . $value);
+
+    });
+});
+
+$botman->hears('Luis', function (BotMan $bot) {
+   $bot->reply('Hi there!');
+});
