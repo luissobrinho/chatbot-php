46447a434d96084852976e07d6051845eddd89fb
Luis Eduardo Altino <ads.luis.sobrinho@gmail.com>
2021-12-21 23:26:03 -0300

    Add new todo

--- routes/botman-todo.php
+++ routes/botman-todo.php
@@ -3,6 +3,8 @@
 use App\Http\Conversations\ButtonConversation;
 use App\Models\Todo;
 use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Messages\Conversations\Conversation;
+use BotMan\BotMan\Messages\Incoming\Answer;
 
 /** @var BotMan $botman */
 $botman = app('botman');
@@ -17,4 +19,21 @@ $botman->hears('show my todos', function (BotMan $bot) {
     } else {
         $bot->reply('You do not have any todos.');
     }
+});
+
+$botman->hears("add new todo (.*)", function (BotMan $bot, $task) {
+   Todo::create([
+       'task' => $task,
+   ]);
+   $bot->reply("You added a new todo for '{$task}'");
+});
+
+$botman->hears("add new todo", function (BotMan $bot) {
+    $bot->ask('Which task do you want to add?', function (Answer $answer, Conversation $conversation) {
+        Todo::create([
+            'task' => $answer,
+        ]);
+        $conversation->say("You added a new todo for '{$answer}'");
+    });
+
 });
